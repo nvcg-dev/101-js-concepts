9f10b5e5fd3c1b781c2a13973db420d2d988ab42
nvcg-dev <nickvcg0@gmail.com>
2025-01-13 11:59:50 -0500

    function closure practice

---
+++ functions/closure-counter.js
@@ -0,0 +1,11 @@
+function createCounter() {
+    let count = 0; // Private variable
+    return function() {
+        count++;
+        return count;
+    };
+}
+
+const counter = createCounter();
+console.log(createCounter()); // Output: 1
+console.log(counter()); // Output: 2
